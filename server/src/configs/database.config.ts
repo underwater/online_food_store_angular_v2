2d274c143240d5b844a4ab631fe9caa4e5cde064
Fares Eidi <fares.eidi@gmail.com>
2024-01-30 09:00:38 +0100

    add docker, fix version of toaster

--- server/src/configs/database.config.ts
+++ server/src/configs/database.config.ts
@@ -1,10 +1,14 @@
 import { connect, ConnectOptions } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const dbConnect = () => {
+
+    console.log(`Connecting to ${process.env.CONNECTION_STRING}...`);
+
     connect(process.env.CONNECTION_STRING!, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: "Online_Food_Store_Database_v2_1"
+        useUnifiedTopology: true
     } as ConnectOptions).then(
         () => console.log("MongoDb Database connect successfully"),
         (error) => console.log('Database connection error', error)
